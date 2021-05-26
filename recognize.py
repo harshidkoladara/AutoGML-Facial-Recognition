5eaf629271ea0451c5207d874b98eba4ecaaaa33
Harshid Koladara <59229200+harshidkoladara@users.noreply.github.com>
2021-05-26 21:55:43 +0530

    Add files via upload

---
+++ recognize.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+import pickle
+
+
+filename = 'finalized_model (1).sav'
+model = pickle.load(open(filename, 'rb'))
+encodeListKnown, className = model[0], model[1]
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25, )
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = className[matchIndex].upper()
+
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2),
+                          (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0))
+            print(name)
+    cv2.imshow('FACE', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
